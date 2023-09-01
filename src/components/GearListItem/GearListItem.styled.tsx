023faf45037b4848ce88aece6d52d7e55d4adf98
Niels Louwes <76694402+NielsLouwes@users.noreply.github.com>
2023-09-01 12:22:25 +0000

    Fixed hover behavior gear list

--- src/components/GearListItem/GearListItem.styled.tsx
+++ src/components/GearListItem/GearListItem.styled.tsx
@@ -2,12 +2,26 @@ import styled from "styled-components";
 
 export const DeleteButton = styled.button`
   border: none;
-  color: #fc5201;
+  color: transparent;
   background: none;
+  cursor: pointer;
+
+  &:hover {
+    color: #fc5201;
+  }
 `;
 
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 25px;
+  padding: 5px 10px;
+
+  &:hover {
+    background: #F7F7FA;
+  }
+
+  &:hover ${DeleteButton} {
+    color: #fc5201;
+  }
 `;
